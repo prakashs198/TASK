ab46ce400ec407d91ace2364a339a4b7dcae0dac
prakashs198 <sharabha198@gmail.com>
2024-06-07 17:57:08 +0530

    Add files via upload

---
+++ task7/src/main/java/com/example/table/repository/Table1Repository.java
@@ -0,0 +1,7 @@
+package com.example.table.repository;
+
+import com.example.table.entity.Table1;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Table1Repository extends JpaRepository<Table1, Long> {
+}
---
+++ task7/src/main/java/com/example/table/repository/Table2Repository.java
@@ -0,0 +1,7 @@
+package com.example.table.repository;
+
+import com.example.table.entity.Table2;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Table2Repository extends JpaRepository<Table2, Long> {
+}
